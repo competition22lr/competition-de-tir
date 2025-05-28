af576e705a7bcd0874e0549492cdb5802b0c20a0
Sylvain <competition22lr@trepanier.cc>
2025-05-27 23:27:42 -0400

    not console log in prod

--- src/app/components/reglements/categories/categories.component.ts
+++ src/app/components/reglements/categories/categories.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-reglement-categories',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css', './../reglements.component.css']
 })
